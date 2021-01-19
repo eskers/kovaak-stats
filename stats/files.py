e0f57475612bfd835af0d0f771bd07715c82ec14
Eskers <77648853+eskers@users.noreply.github.com>
2021-01-19 01:02:10 +0000

    Added structure and files.py

---
+++ stats/files.py
@@ -0,0 +1,84 @@
+"""Module to parse Kovaak stat files and save data which is useful for displaying"""
+
+import os
+import csv
+import datetime
+
+
+def scenario_file_paths(path, scenarios):
+    """
+    Return a dictionary containing list of paths of statistics files, scenarios are
+    used as the keys, paths are a list for the given keys.
+
+    Keyword arguments:
+    path -- string of the path to directory where Kovaak's stores statistics files
+    scenario -- tuple of scenarios to generate list of file paths for
+    """
+    scenario_files = {}
+    file_list = os.listdir(path)
+    for scenario in scenarios:
+        relevant_files = []
+        for file in file_list:
+            if file.startswith(scenario):
+                relevant_files.append(path + file)
+        scenario_files[scenario] = relevant_files
+
+    return scenario_files
+
+
+def get_stats(scenario_files):
+    """
+    Open Kovaak scenario csv stat files and returns a list of tuples containing datetime
+    and the score.
+
+    Keyword arguments:
+    scenario_files -- list of paths for stat files to be read by function
+    """
+    scenarios_stats = []
+    for scenario in scenario_files:
+        date_str = scenario[-29:-19]
+        datetime_str = date_str + " 00:00:00.0"
+        challenge_score = -1.0
+        with open(scenario, newline="") as csvfile:
+            stat_reader = csv.reader(csvfile, dialect="excel")
+            for row in stat_reader:
+                if row and row[0] == "Score:":
+                    challenge_score = float(row[1])
+                if row and row[0] == "Challenge Start:":
+                    datetime_str = datetime_str[:11] + row[1]
+
+        datetime_obj = datetime.datetime.strptime(datetime_str, "%Y.%m.%d %H:%M:%S.%f")
+        challenge_stats = (datetime_obj, challenge_score)
+        scenarios_stats.append(challenge_stats)
+        print(type(challenge_score))
+
+    return scenarios_stats
+
+
+# Temp variables for testing
+STATS_DIR = "/mnt/c/Program Files (x86)/Steam/steamapps/common/FPSAimTrainer/FPSAimTrainer/stats/"
+int_benchmark_scenarios = (
+    "Pasu Voltaic Easy",
+    "B180 Voltaic Easy",
+    "Popcorn Voltaic Easy",
+    "ww3t Voltaic",
+    "1w4ts Voltaic",
+    "6 Sphere Hipfire Voltaic",
+    "Smoothbot Voltaic Easy",
+    "Air Angelic 4 Voltaic Easy",
+    "PGTI Voltaic Easy",
+    "FuglaaXYZ Voltaic Easy",
+    "Ground Plaza Voltaic Easy",
+    "Air Voltaic Easy",
+    "patTS Voltaic Easy",
+    "psalmTS Voltaic Easy",
+    "voxTS Voltaic Easy",
+    "kinTS Voltaic Easy",
+    "B180T Voltaic Easy",
+    "Smoothbot TS Voltaic Easy",
+)
+
+# Test code
+kovaak_stats = scenario_file_paths(STATS_DIR, int_benchmark_scenarios)
+a = get_stats(kovaak_stats["patTS Voltaic Easy"])
+print(a)
